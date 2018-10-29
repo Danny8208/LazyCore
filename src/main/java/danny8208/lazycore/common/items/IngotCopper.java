b7ce94aee4177414607a16d05ab56778cc0aeb59
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2018-10-29 19:42:48 -0400

    imported stuff
    because i did the code in school

--- src/main/java/danny8208/lazycore/common/items/IngotCopper.java
+++ src/main/java/danny8208/lazycore/common/items/IngotCopper.java
@@ -1,5 +1,9 @@
 package danny8208.lazycore.common.items;
 
+import danny8208.lazycore.api.item.ItemBase;
+import danny8208.lazycore.client.CoreTab;
+import danny8208.lazycore.common.LazyCore;
+
 public class IngotCopper extends ItemBase {
   public IngotCopper() {
     super(LazyCore.MOD_ID, "copper_ingot", CoreTab.tab);
--- src/main/java/danny8208/lazycore/common/items/IngotTin.java
+++ src/main/java/danny8208/lazycore/common/items/IngotTin.java
@@ -1,5 +1,9 @@
 package danny8208.lazycore.common.items;
 
+import danny8208.lazycore.api.item.ItemBase;
+import danny8208.lazycore.client.CoreTab;
+import danny8208.lazycore.common.LazyCore;
+
 public class IngotTin extends ItemBase {
 	public IngotTin() {
 		super(LazyCore.MOD_ID, "tin_ingot", CoreTab.tab);
