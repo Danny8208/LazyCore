b629636ed9725955f21466b1c1b74073bba97de3
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2018-10-29 09:58:01 -0400

    Update CoreConfig.java

--- src/main/java/danny8208/lazycore/common/CoreConfig.java
+++ src/main/java/danny8208/lazycore/common/CoreConfig.java
@@ -6,6 +6,25 @@ import org.apache.logging.log4j.Level;
 public class CoreConfig {
     public static boolean enableBronzeIngot = true;
     public static boolean enableTinIngot = true;
+    public static boolean enableCopperIngot = true;
+    public static boolean enableTungstenIngot = true;
+    public static boolean enableSteelIngot = true;
+    
+    public static boolean enableBronzeNugget = true;
+    public static boolean enableTinNugget = true;
+    public static boolean enableCopperNugget = true;
+    public static boolean enableTungstenNugget = true;
+    public static boolean enableSteelNugget = true;
+    
+    public static boolean enableBronzeBlock = true;
+    public static boolean enableTinBlock = true;
+    public static boolean enableCopperBlock = true;
+    public static boolean enableTungstenBlock = true;
+    public static boolean enableSteelBlock = true;
+    
+    public static boolean enableCopperOre = true;
+    public static boolean enableTinOre = true;
+    public static boolean enableTungstenOre = true;
 
     public static void readConfig() {
         Configuration cfg = CommonProxy.config;
