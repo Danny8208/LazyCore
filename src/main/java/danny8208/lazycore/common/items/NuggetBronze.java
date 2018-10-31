36bc121f7bc242be867ed7294f22a02fc02abb3b
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2018-10-31 06:43:34 -0400

    Add files via upload

---
+++ src/main/java/danny8208/lazycore/common/items/NuggetBronze.java
@@ -0,0 +1,11 @@
+package danny8208.lazycore.common.items;
+
+import danny8208.lazycore.api.item.ItemBase;
+import danny8208.lazycore.client.CoreTab;
+import danny8208.lazycore.common.LazyCore;
+
+public class NuggetBronze extends ItemBase {
+    public NuggetBronze() {
+        super(LazyCore.MOD_ID, "bronze_nugget", CoreTab.tab);
+    }
+}
