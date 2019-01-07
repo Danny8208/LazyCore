d37237a088120bb5833be7c757ce18bb5a9bee97
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-01-07 14:45:19 -0500

    Update CraftingRecipes.java

--- src/main/java/danny8208/lazycore/common/CraftingRecipes.java
+++ src/main/java/danny8208/lazycore/common/CraftingRecipes.java
@@ -2,7 +2,11 @@ package danny8208.lazycore.common;
 
 public class CraftingRecipes {
   public void nuggetIngot() {
-    
+    NormalRecipes.addShapelessRecipe(new ItemStack(ModItems.nuggetTungsten, 9), "ingotTungsten");
+    NormalRecipes.addShapelessRecipe(new ItemStack(ModItems.nuggetCopper, 9), "ingotCopper");
+    NormalRecipes.addShapelessRecipe(new ItemStack(ModItems.nuggetBronze, 9), "ingotBronze");
+    NormalRecipes.addShapelessRecipe(new ItemStack(ModItems.nuggetSteel, 9), "ingotSteel");
+    NormalRecipes.addShapelessRecipe(new ItemStack(ModItems.nuggetTin, 9), "ingotTin");
   }
   
   public void ingotNugget() {
