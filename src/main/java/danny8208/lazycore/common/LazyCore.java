f30870076d338350aee7f67207ca4e5850a13a8e
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-01-25 19:25:57 -0500

    updated files for modding for new version of forge

--- src/main/java/danny8208/lazycore/common/LazyCore.java
+++ src/main/java/danny8208/lazycore/common/LazyCore.java
@@ -17,7 +17,7 @@ public class LazyCore {
     public static final String MOD_ID = "lazycore";
     public static final String NAME = "LazyCore";
     public static final String VERSION = "1.0";
-    public static final String FORGE = "14.23.5.2807";
+    public static final String FORGE = "14.23.5.2808";
     public static Logger logger;
 
     @Mod.Instance
